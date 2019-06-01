8e42ccae409cc22f91bdb19459b02fa2815fe11d
xiaran <xiaran@polomi.com>
2019-06-01 14:58:59 +0800

    docker config change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#docker build -t kkb-frontend git@gitlab.kaikeba.com:web_dev/docker_ci.git
-# git pull
+#docker build -t kkb-frontend git@github.com:kkbjs/vue-docker.git
+git pull
 docker build -t kkb-frontend .
 docker stop kkb-frontend
 docker rm kkb-frontend
